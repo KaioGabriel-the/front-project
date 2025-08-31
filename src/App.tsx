f85af646bce6ea447fec972ae8554e0582d205c4
KaioGabriel-the <kkaiogabrielk@gmail.com>
2025-08-31 15:29:13 -0300

    🎉 Commit inicial.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Ambience from './pages/Ambience'
+
+function App() {
+
+  return (
+    <>
+      <Ambience></Ambience>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Ambience/Ambience.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../components/Header";
+
+const Ambience: React.FC = () => {
+  return (
+    <div>
+      <Header title="Ambientes"/>
+    </div>
+  );
+};
+
+export default Ambience;
